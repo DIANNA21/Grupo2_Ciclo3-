584c0bab7e1fdc60feef08b208edee2bb83cfc9e
ACLXRD <andres.lopez01@correo.usa.edu.co>
2021-09-29 21:27:56 -0500

    Se trabaja en el print 2. Funciones creadas y validadas pero no integradas con registro.html

---
+++ validaciones.js
@@ -0,0 +1,74 @@
+function validar_nombre_usuario(params) {
+    let isValid;
+    // El tamaño maximo para nuestro input
+    const max = 30;
+    const min = 6;
+
+    // El pattern que vamos a comprobar
+    const pattern = new RegExp('^[A-Z]+$', 'i');
+
+    // Primera validacion, si input esta vacio entonces no es valido
+    if(!params) {
+      isValid = false;
+    } else {
+      // Segunda validacion, si input es mayor que 30
+      if(params.length > max || params.length < min) {
+        isValid = false;
+      } else {
+        // Tercera validacion, si input contiene caracteres diferentes a los permitidos
+        if(!pattern.test(params)){ 
+        // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
+        // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
+          isValid = false;
+        } else {
+          // Si pasamos todas la validaciones anteriores, entonces el input es valido
+          isValid = true;
+        }
+      }
+    }
+    return isValid;
+}
+
+function validar_contrasena(params) {
+    let isValid;
+    // El tamaño maximo para nuestro input
+    const min = 6;
+
+    // El pattern que vamos a comprobar
+    const pattern = new RegExp('^[a-zA-Z0-9]+$', 'i');
+
+    // Primera validacion, si input esta vacio entonces no es valido
+    if(!params) {
+      isValid = false;
+    } else {
+      // Segunda validacion, si input es mayor que 30
+      if(params.length < min) {
+        isValid = false;
+      } else {
+        // Tercera validacion, si input contiene caracteres diferentes a los permitidos
+        if(!pattern.test(params)){ 
+        // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
+        // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
+          isValid = false;
+        } else {
+          // Si pasamos todas la validaciones anteriores, entonces el input es valido
+          isValid = true;
+        }
+      }
+    }
+    return isValid;
+}
+
+function confirmar_contrasena(param1, param2) {
+    let isValid;
+    validar_contrasena(param1) && validar_contrasena(param2) && param1 == param2 ? isValid = true : isValid = false;
+    return isValid
+}
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_contrasena = validar_contrasena;
+module.exports.confirmar_contrasena = confirmar_contrasena;
+
+// confirmar_contrasena("abcde123", "bcde123") ? console.log("Yes") : console.log("Nop");
+
+
